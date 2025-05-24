beda9f78c06f6b6880b92a50510ef5e411e164b9
Omar Hanafy <omar_hanafy@icloud.com>
2025-05-24 23:30:12 +0300

    removed old files

--- lib/src/features/editor-old-working/editor.dart
+++
@@ -1,7 +0,0 @@
-// Editor feature exports
-export 'domain/editor_settings.dart';
-export 'bridge/monaco_bridge.dart';
-export 'presentation/ui/combined_content_widget.dart';
-export 'presentation/ui/monaco_editor_embedded.dart';
-export 'presentation/ui/monaco_editor_info_bar.dart';
-export 'presentation/ui/editor_settings_dialog.dart';
